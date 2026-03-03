beaa34fd22584043a254af34c5e6688f972f4a98
Nathan Whitaker <nathan@deno.com>
2026-03-03 15:16:17 +0100

    fix windows

--- bench-all.sh
+++ bench-all.sh
@@ -7,9 +7,9 @@ for dir in fixtures/*/; do
   name=$(basename "$dir")
   echo "=== $name ==="
   cd "$dir"
-  hyperfine --warmup 5 --runs 20 -N \
-    --prepare "../../prepare.sh" \
-    -n=deno-2.7.1 "$HOME/deno-2.7.1 install" \
+  hyperfine --warmup 2 -N \
+    --prepare "bash ../../prepare.sh" \
+    -n=deno-2.7.1 "$HOME/Documents/Code/misc/deno-2.7.1 install" \
     -n=deno-canary "deno install"
   cd - >/dev/null
 done
